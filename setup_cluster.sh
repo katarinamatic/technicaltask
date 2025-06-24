5202d7ba9ff155ee0fc5d9e544cc28d7b70f8581
Katarina Matic <katarinamatic@Katarinas-MacBook-Pro.local>
2025-06-24 14:45:47 +0200

    tried making pebble work, stuck on challenge http01 error

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -7,12 +7,16 @@ echo "\n--- minikube started ---\n"
 # add traefik and cert-manager helm repo
 helm repo add traefik https://traefik.github.io/charts
 helm repo add jetstack https://charts.jetstack.io
+helm repo add pebble https://jupyterhub.github.io/helm-chart
 helm repo update
 echo "\n--- helm repos added ---\n"
 
-# install traefik kubernetes controller and cert manager
+# install traefik kubernetes controller, cert manager and pebble
 helm install traefik traefik/traefik --namespace traefik --create-namespace
 echo "\n--- traefik installed ---\n"
 
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.18.0 --set crds.enabled=true
 echo "\n--- cert-manager installed ---\n"
+
+helm install pebble pebble/pebble --namespace pebble --create-namespace
+echo "\n--- pebble installed ---\n"
